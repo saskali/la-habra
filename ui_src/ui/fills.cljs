6f4f3a262f8c97f4ed4d4455acfec77c66fe7ccb
Saskia Lindner <saskia798@gmail.com>
2020-01-24 16:14:29 +0100

    Add color sets

--- ui_src/ui/fills.cljs
+++ ui_src/ui/fills.cljs
@@ -1,5 +1,6 @@
 (ns ui.fills)
 
+
 ;; solid colors A
 (def gray "#53535c")
 (def mint "#00baa9")
@@ -11,3 +12,21 @@
 (def pink "#fb5d67")
 (def white "#e4e4e7")
 (def yellow "#fff70f")
+
+
+;; complementary for turquois
+(def blue-set-1
+  {:dark-green "#006E65"
+   :turquoise "#19FFEC"
+   :aquamarine "#00BAAB"
+   :brown-red "#6E2400"
+   :orange-red "#BA3D00"})
+
+
+;; complementary for aquamarine
+(def blue-set-2
+  {:dark-green "#47B38F"
+   :turquoise "#99FFDD"
+   :aquamarine "#7FFFD4"
+   :brown-red "#B34E36"
+   :orange-red "#FF9980"})
