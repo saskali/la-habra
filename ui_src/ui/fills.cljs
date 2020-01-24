b4fb2de147abeab59c6c9297e8c2b41bae744f23
Saskia Lindner <saskia798@gmail.com>
2020-01-24 22:27:22 +0100

    Add color sets

--- ui_src/ui/fills.cljs
+++ ui_src/ui/fills.cljs
@@ -2,16 +2,42 @@
 
 
 ;; solid colors A
-(def gray "#53535c")
-(def mint "#00baa9")
-(def navy "#161f93")
-(def blue "#001752")
-(def midnight "#05061f")
-(def orange "#ffaa37")
-(def br-orange "#f80")
-(def pink "#fb5d67")
+(def gray "#8795AF")
+(def mint "#41D195")
+(def navy "#1D3D59")
+(def blue "#3D8BF2")
+(def midnight "#1E162B")
+(def orange "#F28705")
+(def pink "#A62D65")
 (def white "#e4e4e7")
-(def yellow "#fff70f")
+(def yellow "#FFCD37")
+
+
+;; color set: blue - red - caramel
+(def color-set-1
+  {:navy "#031838"
+   :light-orchid "#D3A0CC"
+   :burgundy "#81001E"
+   :periwinkle "#ACB7F6"
+   :caramel "#D4AB5E"})
+
+
+;; color set: blue - silver - sunshine - brown - black
+(def color-set-2
+  {:navy "#1D3D59"
+   :silver "#F2F2F2"
+   :yellow "#F29F05"
+   :brown "#591902"
+   :black "#0D0D0D"})
+
+
+;; color set: skin - pink - raspberry - purple - dark blue
+(def color-set-3
+  {:skin "#D98289"
+   :pink "#D96277"
+   :raspberry "#A62D65"
+   :purple "#261E59"
+   :dark-blue "#221A40"})
 
 
 ;; complementary for turquois
@@ -30,3 +56,19 @@
    :aquamarine "#7FFFD4"
    :brown-red "#B34E36"
    :orange-red "#FF9980"})
+
+
+;; cold and rainy
+(def gray-set-1
+  {:dark-gray "#606B7F"
+   :metallic "#8795AF"
+   :silver "#EDF3FF"
+   :light-blue "#C5D9FF"})
+
+
+;; mint
+(def mint-set-1
+  {:grass-green "#216B4C"
+   :green "#35AD7B"
+   :mint "#41D195"
+   :light-mint "#4DF7B0"})
